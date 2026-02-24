5f49195d14b042cbb6748ba2acfacfe26c3232ca
investidor2021 <jhony.gafor@gmail.com>
2026-02-24 16:14:07 -0300

    Ajustando o playwright

--- login.py
+++ login.py
@@ -15,15 +15,25 @@ def login_grp(page, usuario, senha):
     page.get_by_text("Entrar", exact=True).click(force=True)
     page.wait_for_load_state("networkidle")
 
-    campo_usuario = page.locator("input[name='username']")
-    campo_usuario.wait_for(state="visible", timeout=60000)
-    campo_usuario.fill(usuario)
+    # Em alguns casos do GRP, é necessário logar duas vezes se a sessão caiu.
+    # Mas se o 1º login der certo, a página já navegou e os campos de login irão "desaparecer" da tela (detached DOM),
+    # o que estava causando o TimeoutError.
+    
+    # Aguarda 3 segundos para o site decidir se vai carregar a tela inicial ou voltar pro login
+    page.wait_for_timeout(3000)
+    
+    try:
+        if page.locator("input[name='username']").is_visible():
+            campo_usuario = page.locator("input[name='username']")
+            campo_usuario.fill(usuario)
 
-    # senha (geralmente é type=password)
-    campo_senha = page.locator("input[type='password']")
-    campo_senha.wait_for(state="visible", timeout=60000)
-    campo_senha.fill(senha)
+            # senha (geralmente é type=password)
+            campo_senha = page.locator("input[type='password']")
+            campo_senha.fill(senha)
 
-    # botão entrar
-    page.get_by_text("Entrar", exact=True).click(force=True)
-    page.wait_for_load_state("networkidle")
+            # botão entrar
+            page.get_by_text("Entrar", exact=True).click(force=True)
+            page.wait_for_load_state("networkidle")
+    except Exception as e:
+        # Se os campos não existem mais, significa que o primeiro login teve sucesso imediato!
+        pass
