cfd445d392fff65e56bc6c84c11dc931b78e87b1
investidor2021 <jhony.gafor@gmail.com>
2026-02-24 15:50:04 -0300

    Ajustando finais

--- playwright_context.py
+++ playwright_context.py
@@ -1,6 +1,6 @@
 from playwright.sync_api import sync_playwright
 
-def criar_pagina(headless=False):
+def criar_pagina(headless=True):
     p = sync_playwright().start()
     browser = p.chromium.launch(headless=headless)
     contexto = browser.new_context()
